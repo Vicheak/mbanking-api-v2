2f9144d82a6632f1dc4da87b0e8e654b8633d431
Vicheak <suonvicheak991@gmail.com>
2024-01-10 16:26:26 +0700

    init project

---
+++ src/main/java/com/vicheak/mbankingapi/MbankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.vicheak.mbankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MbankingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vicheak/mbankingapi/MbankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicheak.mbankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbankingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
